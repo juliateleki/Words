aeea8ca652b4c7f8221f153c925963aa24c979da
Julia Teleki <julia@teleki.net>
2026-03-30 21:52:25 -0500

    Created Xcode Project

---
+++ Words/Words/WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsApp.swift
+//  Words
+//
+//  Created by Julia Teleki on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Words/WordsTests/WordsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordsTests.swift
+//  WordsTests
+//
+//  Created by Julia Teleki on 3/30/26.
+//
+
+import Testing
+@testable import Words
+
+struct WordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
